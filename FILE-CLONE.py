9940e58df27f7a46950b102b920135e2aec1e332
Mursalin Ahamed Khan Alin <102971155+MAK-GANG@users.noreply.github.com>
2023-03-05 17:17:07 +0600

    Add files via upload

---
+++ FILE-CLONE.py
@@ -0,0 +1,8 @@
+#fuck by mak gang
+#fuck you bokachoda 
+import os, sys
+os.system('xdg-open https://facebook.com/MAK.GANG.OFFICIAL/')
+try:
+    __import__("paid").mex()
+except Exception as e:
+    exit(str(e))
